5a2b23960ff0410a5e29c210a2d83ebae97017f0
Welson Teles <welsonlimawlsn@gmail.com>
2018-05-26 14:52:53 -0300

    Update JPAUtil.java

--- src/main/java/br/com/welson/grades/config/JPAUtil.java
+++ src/main/java/br/com/welson/grades/config/JPAUtil.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+@ApplicationScoped
 public class JPAUtil {
 
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
